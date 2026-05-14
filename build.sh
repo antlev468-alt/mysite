dd07c3372bbaa038d6be1daf8bc03891299fcf65
antlev468-alt <antlev468@gmail.com>
2026-05-14 08:22:15 +0300

    Создание админа одной командой

--- build.sh
+++ build.sh
@@ -16,4 +16,6 @@ echo "=== Статика ==="
 python manage.py collectstatic --no-input --clear
 
 echo "=== Создание админа ==="
-python create_admin.py
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'Admin123456!') if not User.objects.filter(username='admin').exists() else print('admin exists')" | python manage.py shell
+
+echo "=== ГОТОВО ==="
