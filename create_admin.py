6a2d41faaf8f2df18892897da1eb81e9b0f84e42
antlev468-alt <antlev468@gmail.com>
2026-05-27 17:18:10 +0300

    startCommand с create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -6,12 +6,13 @@ django.setup()
 
 from django.contrib.auth.models import User
 
-username = os.environ.get('ADMIN_USERNAME', 'admin')
-email = os.environ.get('ADMIN_EMAIL', 'admin@example.com')
-password = os.environ.get('ADMIN_PASSWORD', 'Admin123456!')
+# Удаляем старого админа если есть
+User.objects.filter(username='admin').delete()
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print(f'Суперпользователь {username} создан!')
-else:
-    print(f'Суперпользователь {username} уже существует.')
+# Создаём нового
+User.objects.create_superuser(
+    username='admin',
+    email='admin@example.com',
+    password='Admin123456!'
+)
+print('АДМИН СОЗДАН!')
