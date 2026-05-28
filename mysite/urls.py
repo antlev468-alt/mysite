d0a7991b6f06ec9275fe6d1bc4e574169437b229
antlev468-alt <antlev468@gmail.com>
2026-05-28 13:46:39 +0300

    едирект на кастомный вход

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+def redirect_to_admin_login(request):
+    return redirect('/admin-login/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin/login/', redirect_to_admin_login),
     path('', include('main.urls')),
 ]
 
